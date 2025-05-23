563d460850570a32b2286edbe7de3d0f0cbc8ad4
lelezhao <zhaolele211@gmail.com>
2025-05-23 17:31:55 +0800

    Update google.ts

--- app/lib/modules/llm/providers/google.ts
+++ app/lib/modules/llm/providers/google.ts
@@ -20,12 +20,9 @@ export default class GoogleProvider extends BaseProvider {
       provider: 'Google',
       maxTokenAllowed: 65536,
     },
+    { name: 'gemini-2.5-flash-preview-05-20', label: 'Gemini 2.5 Flash-05-20', provider: 'Google', maxTokenAllowed: 1048576 },
+    { name: 'gemini-2.5-pro-preview-05-06', label: 'Gemini 2.5 Pro', provider: 'Google', maxTokenAllowed: 1048576 },
     { name: 'gemini-2.0-flash-exp', label: 'Gemini 2.0 Flash', provider: 'Google', maxTokenAllowed: 8192 },
-    { name: 'gemini-1.5-flash-002', label: 'Gemini 1.5 Flash-002', provider: 'Google', maxTokenAllowed: 8192 },
-    { name: 'gemini-1.5-flash-8b', label: 'Gemini 1.5 Flash-8b', provider: 'Google', maxTokenAllowed: 8192 },
-    { name: 'gemini-1.5-pro-latest', label: 'Gemini 1.5 Pro', provider: 'Google', maxTokenAllowed: 8192 },
-    { name: 'gemini-1.5-pro-002', label: 'Gemini 1.5 Pro-002', provider: 'Google', maxTokenAllowed: 8192 },
-    { name: 'gemini-exp-1206', label: 'Gemini exp-1206', provider: 'Google', maxTokenAllowed: 8192 },
   ];
 
   async getDynamicModels(
